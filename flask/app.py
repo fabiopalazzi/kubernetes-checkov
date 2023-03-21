a02b97c29d16cea186a0c7136e3ff851b6a6d91c
Fabio <fabio.palazzi@techedgegroup.com>
2023-03-21 10:41:45 +0100

    Fixed sqlalchemy connection params

--- flask/app.py
+++ flask/app.py
@@ -5,10 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql-svc.default.svc.cluster.local:32000/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@mysql-svc.default.svc.cluster.local:3306/mydatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -16,15 +15,17 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
-    def __int__(self, username, email):
+    def __init__(self, username, email):
         self.username = username
         self.email = email
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/test')
 def testdb():
     try:
-        db.session.query(User).all()
+        User.query.filter_by(username=3).all()
         return '<h1>It works.</h1>'
     except Exception as e:
         print(str(e))
@@ -33,7 +34,7 @@ def testdb():
 
 @app.route("/users", methods=["GET"])
 def get_all_notes():
-    users = db.db.query(User).all()
+    users = db.query(User).all()
     data = []
     for user in users:
         item = {
@@ -46,4 +47,4 @@ def get_all_notes():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
